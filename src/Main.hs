57f125019913d4aba7ec94be3b04eb9dead54d2d
sandalbanditten <sandalbanditten@tutanota.com>
2023-01-29 16:13:28 +0100

    Added a main function - now it is no longer a library

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,45 @@
-import Html
+module Main where
 
-myHtml :: Html
-myHtml = html_ "This is the title"
-       $ h1_ "This is the header"
-       <> p_ "This is the paragraph"
+import Html
+import Markup
+import Convert (convert)
+
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
+
+process :: Title -> String -> String
+process title = Html.render . convert title . Markup.parse
+
+confirm :: IO Bool
+confirm =
+  putStrLn "Are you sure? (y/N)" *>
+    getLine >>= \case
+      "y" -> pure True
+      _   -> pure False
+
+whenIO :: IO Bool -> IO () -> IO ()
+whenIO cond action =
+  cond >>= \result ->
+    if result
+      then action
+      else pure ()
 
 main :: IO ()
-main = do
-  putStrLn $ render myHtml
+main = getArgs >>= \case -- get args
+  -- No arguments; read from stdin and write to stdout
+  [] -> do
+    content <- getContents
+    putStrLn (process "Empty title" content)
+
+  -- With input and output file as arguments
+  [input, output] -> do
+    content <- readFile input
+    exists  <- doesFileExist output
+    let
+      writeResult = writeFile output (process input content)
+    if exists
+      then whenIO confirm writeResult
+      else writeResult
+
+  -- Default to printing an error message
+  _ -> putStrLn "Usage: Main [-- <input-file> <output-file>]"
